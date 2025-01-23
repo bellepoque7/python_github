0c5eba943cc9498a41cab779df17d080485acfa4
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2025-01-23 23:51:51 +0900

    Update tips_load.py

--- tips_load.py
+++ tips_load.py
@@ -9,3 +9,15 @@ def get_tips():
 
 def print_get():
     return '일어나세요 용사여'
+
+
+if __name__ == '__main__':
+    print('''
+    1. get_tips: tips 데이터 로드
+    2. print_get "일어나세요 용사여" 출력
+    ''')
+    choice = input()
+    if choice == '1':
+        get_tips()
+    elif choice == '2':
+        print_get()
